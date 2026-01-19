62c6a4892c5ae3cc12eea3d3215a64d913dcb6cd
Chuma <nmeyiswa@gmail.com>
2026-01-19 14:14:59 +0200

    app anotations

--- beneficiary-service/src/main/java/com/zuluflow/beneficiary/BeneficiaryServiceApplication.java
+++ beneficiary-service/src/main/java/com/zuluflow/beneficiary/BeneficiaryServiceApplication.java
@@ -2,12 +2,12 @@ package com.zuluflow.beneficiary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing; // <--- Import
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BeneficiaryServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BeneficiaryServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BeneficiaryServiceApplication.class, args);
+    }
 }
